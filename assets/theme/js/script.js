a8e3a36b4bdd68db7d9ae4580fea1cee05ebbdd5
Rahul Jain <rahuldkjain@gmail.com>
2019-02-03 19:19:38 +0530

    Updated Portfolio
    Links added to home page front

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1002,4 +1002,4 @@
         }
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise Website Builder</a> v4.8.2';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.9.1';document.body.insertBefore(a,document.body.childNodes[0])}}();
